72d676673ce54d95a63e899360cfa58a7ac7ba78
Alvaro <alabgar176@g.educaand.es>
2024-02-23 12:38:39 +0100

    Añadir comentarios a la clase Figura

--- Figura.java
+++ Figura.java
@@ -1,4 +1,16 @@
+/**
+	Representa la estructura basica de figuras geometricas
+	@author Alvaro
+*/
 public abstract class Figura{
+	/**
+	Retorna el area de la figura
+	@return Area como double
+	*/
 	public abstract double area();
+	/**
+	Retorna el perimetro de la figura
+	@return Perimetro como double
+	*/
 	public abstract double perimetro();
 }
